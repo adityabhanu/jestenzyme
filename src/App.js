c042c9e785bafd7fe4d1a0070ec34d8f03f3dbfa
adityabhanu <adityabhanu08@gmail.com>
2021-05-08 21:10:23 +0530

    Start Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
-    <div className="App">
-      <h1>Learn react Testing</h1>
+    <div data-test="component-app">
+      <h1>App</h1>
     </div>
   );
 }
